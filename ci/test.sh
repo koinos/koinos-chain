451e8e164ec7a9072e42940688980052ae7a98f5
theoretical <theoreticalbts@users.noreply.github.com>
2020-05-29 12:42:50 -0400

    Add return code to vmtest.cpp, check in CI #107

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-cd $(dirname "$0")/../build/tests
-exec ctest -j8 --output-on-failure
+set -e
+set -x
+
+SRC_DIR=$(realpath $(dirname "$0")/..)
+BUILD_DIR="$SRC_DIR/build"
+
+# In CI, vmtest is guaranteed to exist if we've gotten to this point (compilation failure bails in build.sh)
+# So the existence check is only applicable when running this script manually
+if [ -e "$BUILD_DIR/programs/vmtest/vmtest" ]
+then
+   cd "$SRC_DIR/programs/vmtest"
+   "$BUILD_DIR/programs/vmtest/vmtest"
+fi
+
+cd "$BUILD_DIR/tests"
+ctest -j8 --output-on-failure
