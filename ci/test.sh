53850c99c57e0273f11cccdac5ca0edf64a7c150
Steve Gerbino <steve@gerbino.co>
2020-06-01 18:45:11 -0400

    #88: Add the vmtest back in to the test build

--- ci/test.sh
+++ ci/test.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+set -e
+set -x
+
 if [ "$RUN_TYPE" = "test" ]; then
-   cd $(dirname "$0")/../build/tests
-   exec ctest -j8 --output-on-failure
+
+   SRC_DIR=$(realpath $(dirname "$0")/..)
+   BUILD_DIR="$SRC_DIR/build"
+
+   # In CI, vmtest is guaranteed to exist if we've gotten to this point (compilation failure bails in build.sh)
+   # So the existence check is only applicable when running this script manually
+   if [ -e "$BUILD_DIR/programs/vmtest/vmtest" ]
+   then
+      cd "$SRC_DIR/programs/vmtest"
+      "$BUILD_DIR/programs/vmtest/vmtest"
+   fi
+
+   cd "$BUILD_DIR/tests"
+   ctest -j8 --output-on-failure
 fi
 
