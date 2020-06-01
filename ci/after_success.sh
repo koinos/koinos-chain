4d5c68dbc80f4124df2e8a90ab41e312be28ba01
Steve Gerbino <steve@gerbino.co>
2020-06-01 16:46:29 -0400

    #88: Add travis-pro service name

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -2,6 +2,6 @@
 
 if [ "$RUN_TYPE" = "coverage" ]; then
    # coveralls -n -t $COVERALLS_REPO_TOKEN --gcov ./build/coverage.info
-   coveralls-lcov --repo-token $COVERALLS_REPO_TOKEN ./build/coverage.info
+   coveralls-lcov --repo-token "$COVERALLS_REPO_TOKEN" --service-name travis-pro ./build/coverage.info
 fi
 
