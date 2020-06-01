cabb1b9a670bae78f221f9cd5be03f668bf47721
Steve Gerbino <steve@gerbino.co>
2020-06-01 18:03:12 -0400

    #88: Cleanup after_success script

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ "$RUN_TYPE" = "coverage" ]; then
-   # coveralls -n -t $COVERALLS_REPO_TOKEN --gcov ./build/coverage.info
    coveralls-lcov --repo-token "$COVERALLS_REPO_TOKEN" --service-name travis-pro ./build/coverage.info
 fi
 
