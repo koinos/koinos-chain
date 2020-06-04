3553c2e7af87ed86fff5a61246f318440dbd1743
Steve Gerbino <steve@gerbino.co>
2020-06-03 22:29:57 -0400

    #77: Make koinos-vm a VM driver program

--- programs/koinos-vm/driver.cpp
+++ programs/koinos-vm/driver.cpp
@@ -1,22 +1,50 @@
-#include <koinos/chain/system_calls.hpp>
+#include <cstddef>
+#include <iostream>
+
+#include <boost/program_options.hpp>
+
 #include <chainbase/chainbase.hpp>
-#include <mira/database_configuration.hpp>
+
+#include <koinos/chain/system_calls.hpp>
 #include <koinos/exception.hpp>
 
-#include <iostream>
-#include <cstddef>
+#include <mira/database_configuration.hpp>
 
-int main(int argc, char** argv, char** envp)
+#define HELP_OPTION     "help"
+#define CONTRACT_OPTION "contract"
+
+int main( int argc, char** argv, char** envp )
 {
    try
    {
+      boost::program_options::options_description desc( "Koinos VM options" );
+      desc.add_options()
+        ( HELP_OPTION ",h", "print usage message" )
+        ( CONTRACT_OPTION ",c", boost::program_options::value< std::string >(), "the contract to run" )
+        ;
+
+      boost::program_options::variables_map vmap;
+      boost::program_options::store( boost::program_options::parse_command_line( argc, argv, desc ), vmap );
+
+      if ( vmap.count( HELP_OPTION ) )
+      {
+         std::cout << desc << std::endl;
+         return EXIT_SUCCESS;
+      }
+
+      if ( !vmap.count( CONTRACT_OPTION ) )
+      {
+         std::cout << desc << std::endl;
+         return EXIT_FAILURE;
+      }
+
       koinos::chain::register_syscalls();
       koinos::chain::wasm_allocator_type wa;
-      std::vector< uint8_t > wasm_bin = koinos::chain::backend_type::read_wasm("hello.wasm");
-      koinos::chain::backend_type bkend( wasm_bin, koinos::chain::registrar_type{} );
+      std::vector< uint8_t > wasm_bin = koinos::chain::backend_type::read_wasm( vmap[ CONTRACT_OPTION ].as< std::string >() );
+      koinos::chain::backend_type backend( wasm_bin, koinos::chain::registrar_type{} );
 
-      bkend.set_wasm_allocator(&wa);
-      bkend.initialize();
+      backend.set_wasm_allocator( &wa );
+      backend.initialize();
 
       chainbase::database db;
       auto tmp = boost::filesystem::current_path() / boost::filesystem::unique_path();
@@ -32,8 +60,10 @@ int main(int argc, char** argv, char** envp)
 
       koinos::chain::system_call_table t;
       koinos::chain::apply_context ctx( db, t );
-      ctx.receiver = koinos::chain::name(0);
-      bkend(&ctx, "env", "apply", (uint64_t)0, (uint64_t)0, (uint64_t)0);
+      ctx.receiver = koinos::chain::name( 0 );
+
+      backend( &ctx, "env", "apply", (uint64_t)0, (uint64_t)0, (uint64_t)0 );
+
       std::cout << ctx.get_pending_console_output() << std::endl;
    }
    catch( const eosio::vm::exception& e )
