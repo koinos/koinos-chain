8ac10133353abd18563fc2689030cc01ce473b61
Steve Gerbino <steve@gerbino.co>
2020-06-03 23:36:58 -0400

    #77: Clean up strpolate test case

--- tests/tests/strpolate_test.cpp
+++ tests/tests/strpolate_test.cpp
@@ -10,8 +10,7 @@ BOOST_AUTO_TEST_CASE( strpolate_tests )
 {
    std::string my_name = "Pythagoras";
    int my_age = 2300;
-   BOOST_CHECK( "My name is Pythagoras, I am 2300 years old." == STRPOLATE( "My name is ${name}, I am ${years} years old.", ("name", my_name)("years", my_age) ) );
-   BOOST_CHECK( "Failure case" != STRPOLATE( "My name is ${name}, I am ${years} years old.", ("name", my_name)("years", my_age) ) );
+   BOOST_CHECK_EQUAL( "My name is Pythagoras, I am 2300 years old.", STRPOLATE( "My name is ${name}, I am ${years} years old.", ("name", my_name)("years", my_age) ) );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
