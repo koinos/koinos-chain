ad5e3bb2ba352d97dc33912c2d3378f19b7aae87
youkaicountry <youkaicountry@users.noreply.github.com>
2020-06-24 13:40:27 -0400

    Fixes

--- libraries/chain/include/koinos/chain/system_calls.hpp
+++ libraries/chain/include/koinos/chain/system_calls.hpp
@@ -3,6 +3,7 @@
 #include <koinos/chain/types.hpp>
 #include <koinos/exception.hpp>
 #include <koinos/pack/rt/basetypes.hpp>
+#include <koinos/pack/classes.hpp>
 
 #include <koinos/statedb/statedb_types.hpp>
 
@@ -12,8 +13,8 @@ DECLARE_KOINOS_EXCEPTION( unknown_system_call );
 
 // First 160 bits are obtained by 160-bit truncation of sha256("object_space:system_call")
 const statedb::object_space SYS_CALL_DISPATCH_TABLE_SPACE_ID = protocol::uint256_t("0xd15cd01c47057163768c9d339a81495e6d167f20000000000000000000000001");
-// 20 bytes contract ID + 1 byte variant
-const int64_t SYS_CALL_DISPATCH_TABLE_OBJECT_MAX_SIZE = 21;
+// Size for buffer when fetching system call from database -> 1 for variant, 20 for contract_id, 4 for 
+const int64_t SYS_CALL_DISPATCH_TABLE_OBJECT_MAX_SIZE = 1 + 20 + 4;
 
 variable_blob get_default_sys_call_entry( uint32_t sid );
 
--- libraries/chain/thunks.cpp
+++ libraries/chain/thunks.cpp
@@ -106,10 +106,11 @@ THUNK_DEFINE( void, apply_execute_contract_operation, ((const protocol::contract
 
 THUNK_DEFINE( void, apply_set_system_call_operation, ((const protocol::set_system_call_operation&) o) )
 {
-   protocol::system_call_bundle sys_call;
-   sys_call.contract_id = o.contract_id;
-   sys_call.entry_point = o.entrypoint;
-   db_put_object( context, 1, o.call_id, pack::to_variable_blob( sys_call ) );
+   protocol::system_call_bundle bundle;
+   bundle.contract_id = o.contract_id;
+   bundle.entry_point = o.entrypoint;
+   protocol::system_call_target s;
+   db_put_object( context, 1, o.call_id, pack::to_variable_blob( bundle ) );
 }
 
 THUNK_DEFINE( bool, db_put_object, ((const statedb::object_space&) space, (const statedb::object_key&) key, (const variable_blob&) obj) )
