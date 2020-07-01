be22c0b3799988c472db28be47666560c89c5bed
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-07-01 13:50:07 -0700

    Pass syscall override #172

--- wasm/tests/syscall_override.c
+++ wasm/tests/syscall_override.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 char prepend[] = "test: ";
+#define PREPEND_LEN sizeof(prepend) - 1
 
 void invoke_system_call( uint32_t sid, char* ret_ptr, uint32_t ret_len, char* arg_ptr, uint32_t arg_len );
 void invoke_thunk( uint32_t tid, char* ret_ptr, uint32_t ret_len, char* arg_ptr, uint32_t arg_len );
@@ -11,7 +12,7 @@ void prints( char* msg )
 
    int i = 0;
 
-   while( i < 6 )
+   while( i < PREPEND_LEN )
    {
       args[i+1] = prepend[i];
       i++;
@@ -19,21 +20,21 @@ void prints( char* msg )
 
    i = 0;
 
-   while( msg[i] && i < 129 - 6 - 1 )
+   while( msg[i] && i < 129 - PREPEND_LEN - 1 )
    {
-      // Purposefully OBO to overwrite 0 at the end of prepend
-      args[i + 6 + 1] = msg[i];
+      args[i+PREPEND_LEN+1] = msg[i];
       i++;
    }
-   args[0] = (uint8_t)i + 6 + 1;
+   args[0] = (uint8_t)(i + PREPEND_LEN);
 
-   invoke_thunk( 0, 0, 0, args, i + 6 + 1 );
+   invoke_thunk( 0, 0, 0, args, i + PREPEND_LEN + 1 );
 }
 
 __attribute__( (visibility("default")) )
 void apply( uint64_t a, uint64_t b, uint64_t c )
 {
-   char message[64];
+   char message[65];
    invoke_system_call( 14, message, 63, 0, 0 );
-   prints( message + 1 );
+   message[ message[0] + 1 ] = 0;
+   prints( message + 2 );
 }
