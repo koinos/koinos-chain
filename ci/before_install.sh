e5d6e2fbc8128f275e616f65690bd77fad58c017
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-07-01 11:49:59 -0700

    Try caching Homebrew #196

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -3,5 +3,13 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
    sudo apt-get update -qq
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-   brew update
+   cd /usr/local/Homebrew/Library/Taps
+   if [ -d homebrew/homebrew-cask ]; then rm -rf caskroom/homebrew-cask; fi
+   for repo in find -type d -name .git; do
+      cd repo/..
+      git clean -fxd
+   done
+
+   brew cleanup
+   brew update --merge
 fi
