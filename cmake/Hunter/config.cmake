92174a51fb5fc2796502e7a0491335363a5541d9
Steve Gerbino <steve@gerbino.co>
2020-12-18 04:44:58 -0500

    #254: Use hunter package manager to resolve RocksDB and nlohmann_json dependencies

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,20 @@
 hunter_config(Boost
-    VERSION ${HUNTER_Boost_VERSION}
-    CMAKE_ARGS USE_CONFIG_FROM_BOOST=ON
+   VERSION ${HUNTER_Boost_VERSION}
+   CMAKE_ARGS USE_CONFIG_FROM_BOOST=ON
+)
+
+hunter_config(rocksdb
+   VERSION ${HUNTER_rocksdb_VERSION}
+   CMAKE_ARGS
+      WITH_TESTS=OFF
+      WITH_TOOLS=OFF
+      WITH_JNI=OFF
+      WITH_BENCHMARK_TOOLS=OFF
+      WITH_CORE_TOOLS=OFF
+      WITH_GFLAGS=OFF
+      PORTABLE=ON
+      FAIL_ON_WARNINGS=OFF
+      CMAKE_CXX_FLAGS=-fvisibility=hidden
+      CMAKE_C_FLAGS=-fvisibility=hidden
 )
 
