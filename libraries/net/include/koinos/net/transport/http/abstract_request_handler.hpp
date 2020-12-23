f01ab7375a18e9d75771b24d9626998ce8769d22
theoretical <theoreticalbts@users.noreply.github.com>
2020-12-23 00:36:58 -0500

    Move code from request_handler.hpp to request_handler.cpp #266

--- libraries/net/include/koinos/net/transport/http/abstract_request_handler.hpp
+++ libraries/net/include/koinos/net/transport/http/abstract_request_handler.hpp
@@ -7,7 +7,9 @@ namespace koinos::net::transport::http {
 struct abstract_request_handler
 {
    virtual std::string handle( const std::string& payload ) = 0;
-   virtual ~abstract_request_handler() {}
+
+   abstract_request_handler();
+   virtual ~abstract_request_handler();
 };
 
 } // koinos::net::transport::http
