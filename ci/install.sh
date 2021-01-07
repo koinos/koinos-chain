2435cbac293d3cdcd33cff262a24a02c99b7fc92
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-01-07 14:51:35 -0800

    Cleanup for review

--- ci/install.sh
+++ ci/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$CC" = "clang-10" ]; then
+if [ "$CC" = "clang-11" ]; then
    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-   sudo apt-add-repository 'deb http://apt.llvm.org/focal/ llvm-toolchain-focal-10 main'
+   sudo apt-add-repository 'deb http://apt.llvm.org/focal/ llvm-toolchain-focal-11 main'
    sudo apt-get install -y llvm-11 clang-11
 fi
 
