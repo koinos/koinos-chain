1cff938e5ddca0b6369d3cc3ac76e9af6f63ac86
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-01-08 12:58:06 -0800

    Pass boost static libs option in to hunter config

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,9 @@
 hunter_config(Boost
    VERSION ${HUNTER_Boost_VERSION}
-   CMAKE_ARGS USE_CONFIG_FROM_BOOST=ON
+   CMAKE_ARGS
+   USE_CONFIG_FROM_BOOST=ON
+   Boost_USE_STATIC_LIBS=ON
+   Boost_NO_BOOST_CMAKE=ON
 )
 
 hunter_config(rocksdb
@@ -18,4 +21,3 @@ hunter_config(rocksdb
       CMAKE_CXX_FLAGS=-fvisibility=hidden
       CMAKE_C_FLAGS=-fvisibility=hidden
 )
-
