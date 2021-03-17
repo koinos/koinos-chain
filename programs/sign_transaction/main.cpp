c7f4df9e820c92856daa958bdd6c6e91990ef2a9
nathan <Nathaniel Caldwell>
2021-03-17 18:07:12 -0400

    Add transaction signing utility function

--- programs/sign_transaction/main.cpp
+++ programs/sign_transaction/main.cpp
@@ -1,13 +1,28 @@
+#include <iostream>
+
 #include <boost/program_options.hpp>
 
 #include <koinos/exception.hpp>
 #include <koinos/log.hpp>
+#include <koinos/crypto/elliptic.hpp>
+#include <koinos/crypto/multihash.hpp>
+#include <koinos/pack/classes.hpp>
+
+void sign_transaction( koinos::protocol::transaction& transaction, koinos::crypto::private_key& transaction_signing_key )
+{
+   // Signature is on the hash of the active data
+   koinos::multihash digest = koinos::crypto::hash( CRYPTO_SHA2_256_ID, transaction.active_data );
+   auto signature = transaction_signing_key.sign_compact( digest );
+   koinos::pack::to_variable_blob( transaction.signature_data, signature );
+}
 
 int main( int argc, char** argv )
 {
    try
    {
       boost::program_options::options_description options;
+
+      std::cout << std::cin.rdbuf();
       
       return EXIT_SUCCESS;
    }
