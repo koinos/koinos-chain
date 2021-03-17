2959f786cb684edf8f8a97f12cfe17e847525146
nathan <Nathaniel Caldwell>
2021-03-17 16:17:31 -0400

    Add basic transaction signing tool

---
+++ programs/sign_transaction/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <string>
+
+#include <boost/program_options.hpp>
+
+#include <koinos/exception.hpp>
+#include <koinos/log.hpp>
+
+int main( int argc, char** argv )
+{
+   try
+   {
+      boost::program_options::options_description options;
+      
+      return EXIT_SUCCESS;
+   }
+   catch ( const boost::exception& e )
+   {
+      LOG(fatal) << boost::diagnostic_information( e ) << std::endl;
+   }
+   catch ( const std::exception& e )
+   {
+      LOG(fatal) << e.what() << std::endl;
+   }
+   catch ( ... )
+   {
+      LOG(fatal) << "unknown exception" << std::endl;
+   }
+
+   return EXIT_FAILURE;
+}
