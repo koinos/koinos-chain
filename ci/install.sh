8a4294d61cbb64c0cd397b407a19129325e6258b
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-04-28 12:10:59 -0700

    Use downgrade work-around

--- ci/install.sh
+++ ci/install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 if [[ -z $BUILD_DOCKER ]]; then
-   if [ "$CC" = "clang-11" ]; then
-      wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-      sudo apt-add-repository 'deb http://apt.llvm.org/focal/ llvm-toolchain-focal-11 main'
-      sudo apt-get install -y llvm-11 clang-11
-   fi
+   sudo apt-get install -yq --allow-downgrades libc6=2.31-0ubuntu9.2 libc6-dev=2.31-0ubuntu9.2
+   sudo -E apt-get -yq --no-install-suggests --no-install-recommends --allow-downgrades --allow-remove-essential --allow-change-held-packages install clang-11 llvm-11 -o Debug::pkgProblemResolver=yes
 
    if [ "$RUN_TYPE" = "coverage" ]; then
       sudo apt-get install -y lcov ruby
