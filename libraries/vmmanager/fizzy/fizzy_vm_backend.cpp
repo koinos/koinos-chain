662546d361ad963c7907d1de22b51afb514c6224
theoreticalbts <theoreticalbts@users.noreply.github.com>
2021-08-03 01:17:15 +0000

    Refactor and get exceptions working

--- libraries/vmmanager/fizzy/fizzy_vm_backend.cpp
+++ libraries/vmmanager/fizzy/fizzy_vm_backend.cpp
@@ -6,6 +6,7 @@
 #include <koinos/vmmanager/fizzy/exceptions.hpp>
 #include <koinos/vmmanager/fizzy/fizzy_vm_backend.hpp>
 
+#include <exception>
 #include <optional>
 #include <string>
 
@@ -70,99 +71,72 @@ const char* fizzy_error_code_name(FizzyErrorCode code) noexcept
    }
 }
 
-void fizzy_vm_backend::run( context& ctx, char* bytecode_data, size_t bytecode_size )
+class fizzy_runner
+{
+   public:
+      fizzy_runner( context& c ) : _ctx(c) {}
+      ~fizzy_runner();
+
+      void parse_bytecode( char* bytecode_data, size_t bytecode_size );
+      void instantiate_module();
+      void call_start();
+
+      FizzyExecutionResult _invoke_thunk( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+      FizzyExecutionResult _invoke_system_call( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+
+   private:
+      context& _ctx;
+      const FizzyModule* _module = nullptr;
+      FizzyInstance* _instance = nullptr;
+      std::exception_ptr _exception;
+};
+
+fizzy_runner::~fizzy_runner()
+{
+   if( _instance != nullptr )
+   {
+      fizzy_free_instance( _instance );
+      _instance = nullptr;
+      // As per fizzy docs, don't free _module if _instance was created successfully
+      _module = nullptr;
+   }
+   else
+   {
+      if( _module != nullptr )
+      {
+         fizzy_free_module( _module );
+      }
+   }
+}
+
+void fizzy_runner::parse_bytecode( char* bytecode_data, size_t bytecode_size )
 {
    // TODO:  Add a caching system to recycle parsed modules
-   const FizzyModule* module = fizzy_parse((uint8_t *) bytecode_data, bytecode_size, nullptr);
-   KOINOS_ASSERT( module != nullptr, module_parse_exception, "Could not parse Fizzy module" );
+   _module = fizzy_parse((uint8_t *) bytecode_data, bytecode_size, nullptr);
+   KOINOS_ASSERT( _module != nullptr, module_parse_exception, "Could not parse Fizzy module" );
+}
 
+void fizzy_runner::instantiate_module()
+{
    FizzyExternalFn invoke_thunk = [](void* voidptr_context, FizzyInstance* fizzy_instance, const FizzyValue* args, FizzyExecutionContext* fizzy_context) noexcept -> FizzyExecutionResult
    {
-      FizzyExecutionResult result;
-      result.trapped = true;
-      result.has_value = false;
-      result.value.i64 = 0;
-      try
-      {
-         context* pctx = static_cast<context *>(voidptr_context);
-         uint32_t tid = args[0].i32;
-         uint32_t ret_len = args[2].i32;
-         char* ret_ptr = resolve_ptr(fizzy_instance, args[1].i32, ret_len);
-         uint32_t arg_len = args[4].i32;
-         const char* arg_ptr = resolve_ptr(fizzy_instance, args[3].i32, arg_len);
-
-         KOINOS_ASSERT( ret_ptr != nullptr, wasm_memory_exception, "Invalid ret_ptr in invoke_thunk()" );
-         KOINOS_ASSERT( arg_ptr != nullptr, wasm_memory_exception, "Invalid arg_ptr in invoke_thunk()" );
-
-         pctx->_chain_host_api->invoke_thunk( tid, ret_ptr, ret_len, arg_ptr, arg_len );
-         result.trapped = false;
-      }
-      catch( const koinos::exception& e )
-      {
-         LOG(warning) << e.get_message() << std::endl;
-      }
-      catch ( const boost::exception& e )
-      {
-         LOG(warning) << boost::diagnostic_information( e ) << std::endl;
-      }
-      catch ( const std::exception& e )
-      {
-         LOG(warning) << e.what() << std::endl;
-      }
-      catch ( ... )
-      {
-         LOG(warning) << "Unknown exception" << std::endl;
-      }
-      return result;
-   };
-
-   FizzyExternalFn invoke_system_call = [](void* voidptr_context, FizzyInstance* fizzy_instance, const FizzyValue* args, FizzyExecutionContext* fizzy_context) noexcept -> FizzyExecutionResult
-   {
-      FizzyExecutionResult result;
-      result.trapped = true;
-      result.has_value = false;
-      result.value.i64 = 0;
-      try
-      {
-         context* pctx = static_cast<context *>(voidptr_context);
-         uint32_t xid = args[0].i32;
-         uint32_t ret_len = args[2].i32;
-         char* ret_ptr = resolve_ptr(fizzy_instance, args[1].i32, ret_len);
-         uint32_t arg_len = args[4].i32;
-         const char* arg_ptr = resolve_ptr(fizzy_instance, args[3].i32, arg_len);
-
-         KOINOS_ASSERT( ret_ptr != nullptr, wasm_memory_exception, "Invalid ret_ptr in invoke_system_call()" );
-         KOINOS_ASSERT( arg_ptr != nullptr, wasm_memory_exception, "Invalid arg_ptr in invoke_system_call()" );
-
-         pctx->_chain_host_api->invoke_system_call( xid, ret_ptr, ret_len, arg_ptr, arg_len );
-         result.trapped = false;
-      }
-      catch( const koinos::exception& e )
-      {
-         LOG(warning) << e.get_message() << std::endl;
-      }
-      catch ( const boost::exception& e )
-      {
-         LOG(warning) << boost::diagnostic_information( e ) << std::endl;
-      }
-      catch ( const std::exception& e )
-      {
-         LOG(warning) << e.what() << std::endl;
-      }
-      catch ( ... )
-      {
-         LOG(warning) << "Unknown exception" << std::endl;
-      }
-      return result;
+      fizzy_runner* runner = static_cast< fizzy_runner* >( voidptr_context );
+      return runner->_invoke_thunk( args, fizzy_context );
    };
 
    FizzyValueType invoke_thunk_arg_types[] = {FizzyValueTypeI32, FizzyValueTypeI32, FizzyValueTypeI32, FizzyValueTypeI32, FizzyValueTypeI32};
    size_t invoke_thunk_num_args = 5;
-   FizzyExternalFunction invoke_thunk_fn = {{ FizzyValueTypeVoid, invoke_thunk_arg_types, invoke_thunk_num_args }, invoke_thunk, &ctx };
+   FizzyExternalFunction invoke_thunk_fn = {{ FizzyValueTypeVoid, invoke_thunk_arg_types, invoke_thunk_num_args }, invoke_thunk, this };
+
+   FizzyExternalFn invoke_system_call = [](void* voidptr_context, FizzyInstance* fizzy_instance, const FizzyValue* args, FizzyExecutionContext* fizzy_context) noexcept -> FizzyExecutionResult
+   {
+      fizzy_runner* runner = static_cast< fizzy_runner* >( voidptr_context );
+      return runner->_invoke_system_call( args, fizzy_context );
+   };
 
    FizzyValueType invoke_system_call_arg_types[] = {FizzyValueTypeI32, FizzyValueTypeI32, FizzyValueTypeI32, FizzyValueTypeI32, FizzyValueTypeI32};
    size_t invoke_system_call_num_args = 5;
-   FizzyExternalFunction invoke_system_call_fn = {{ FizzyValueTypeVoid, invoke_system_call_arg_types, invoke_system_call_num_args }, invoke_system_call, &ctx };
+   FizzyExternalFunction invoke_system_call_fn = {{ FizzyValueTypeVoid, invoke_system_call_arg_types, invoke_system_call_num_args }, invoke_system_call, this };
 
    size_t num_host_funcs = 2;
    FizzyImportedFunction host_funcs[] = {{"env", "invoke_thunk", invoke_thunk_fn}, {"env", "invoke_system_call", invoke_system_call_fn}};
@@ -172,20 +146,97 @@ void fizzy_vm_backend::run( context& ctx, char* bytecode_data, size_t bytecode_s
    // TODO:  Make memory_pages_limit configurable
    size_t memory_pages_limit = 512;     // Number of 64k pages allowed to allocate
 
-   FizzyInstance* instance = fizzy_resolve_instantiate(module, host_funcs, num_host_funcs, nullptr, nullptr, nullptr, 0, memory_pages_limit, &fizzy_err);
-   if( instance == nullptr )
+   _instance = fizzy_resolve_instantiate(_module, host_funcs, num_host_funcs, nullptr, nullptr, nullptr, 0, memory_pages_limit, &fizzy_err);
+   if( _instance == nullptr )
    {
       std::string error_code = fizzy_error_code_name( fizzy_err.code );
       std::string error_message = fizzy_err.message;
       KOINOS_THROW( module_instantiate_exception, "Could not instantiate module - ${code}: ${msg}", ("code", error_code)("msg", error_message) );
    }
+}
 
+FizzyExecutionResult fizzy_runner::_invoke_thunk( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept
+{
+   FizzyExecutionResult result;
+   result.has_value = false;
+   result.value.i64 = 0;
+
+   _exception = std::exception_ptr();
+
+   try
+   {
+      uint32_t tid = args[0].i32;
+      uint32_t ret_len = args[2].i32;
+      char* ret_ptr = resolve_ptr(_instance, args[1].i32, ret_len);
+      uint32_t arg_len = args[4].i32;
+      const char* arg_ptr = resolve_ptr(_instance, args[3].i32, arg_len);
+
+      KOINOS_ASSERT( ret_ptr != nullptr, wasm_memory_exception, "Invalid ret_ptr in invoke_thunk()" );
+      KOINOS_ASSERT( arg_ptr != nullptr, wasm_memory_exception, "Invalid arg_ptr in invoke_thunk()" );
+
+      _ctx._chain_host_api->invoke_thunk( tid, ret_ptr, ret_len, arg_ptr, arg_len );
+   }
+   catch(...)
+   {
+      _exception = std::current_exception();
+   }
+
+   result.trapped = !!_exception;
+   return result;
+}
+
+FizzyExecutionResult fizzy_runner::_invoke_system_call( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept
+{
+   FizzyExecutionResult result;
+   result.has_value = false;
+   result.value.i64 = 0;
+
+   _exception = std::exception_ptr();
+
+   try
+   {
+      uint32_t xid = args[0].i32;
+      uint32_t ret_len = args[2].i32;
+      char* ret_ptr = resolve_ptr(_instance, args[1].i32, ret_len);
+      uint32_t arg_len = args[4].i32;
+      const char* arg_ptr = resolve_ptr(_instance, args[3].i32, arg_len);
+
+      KOINOS_ASSERT( ret_ptr != nullptr, wasm_memory_exception, "Invalid ret_ptr in invoke_thunk()" );
+      KOINOS_ASSERT( arg_ptr != nullptr, wasm_memory_exception, "Invalid arg_ptr in invoke_thunk()" );
+
+      _ctx._chain_host_api->invoke_system_call( xid, ret_ptr, ret_len, arg_ptr, arg_len );
+   }
+   catch(...)
+   {
+      _exception = std::current_exception();
+   }
+
+   result.trapped = !!_exception;
+   return result;
+}
+
+void fizzy_runner::call_start()
+{
    uint32_t start_func_idx = 0;
-   bool success = fizzy_find_exported_function_index( module, "_start", &start_func_idx );
+   bool success = fizzy_find_exported_function_index( _module, "_start", &start_func_idx );
    KOINOS_ASSERT( success, module_start_exception, "Module does not have _start function" );
 
-   FizzyExecutionResult result = fizzy_execute( instance, start_func_idx, nullptr );
-   // KOINOS_ASSERT( !result.trapped, wasm_trap_exception, "Module exited due to trap" );
+   FizzyExecutionResult result = fizzy_execute( _instance, start_func_idx, nullptr );
+   if( _exception )
+   {
+      std::exception_ptr exc = _exception;
+      _exception = std::exception_ptr();
+      std::rethrow_exception( exc );
+   }
+   KOINOS_ASSERT( !result.trapped, wasm_trap_exception, "Module exited due to trap" );
+}
+
+void fizzy_vm_backend::run( context& ctx, char* bytecode_data, size_t bytecode_size )
+{
+   fizzy_runner runner(ctx);
+   runner.parse_bytecode( bytecode_data, bytecode_size );
+   runner.instantiate_module();
+   runner.call_start();
 }
 
 }
