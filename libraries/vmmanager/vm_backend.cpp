83360730ff59c8d8dd07ffe4af25a5e49961afac
theoreticalbts <theoreticalbts@users.noreply.github.com>
2021-08-04 15:56:18 +0000

    Allow setting backend via KOINOS_VM_BACKEND environment variable

--- libraries/vmmanager/vm_backend.cpp
+++ libraries/vmmanager/vm_backend.cpp
@@ -5,6 +5,8 @@
 #include <koinos/vmmanager/eos/eos_vm_backend.hpp>
 #include <koinos/vmmanager/fizzy/fizzy_vm_backend.hpp>
 
+#include <cstdlib>
+
 namespace koinos::vmmanager {
 
 vm_backend::vm_backend() {}
@@ -22,9 +24,23 @@ std::vector< std::shared_ptr< vm_backend > > get_vm_backends()
 
 std::string default_vm_backend = "fizzy";
 
+std::string get_default_vm_backend_name()
+{
+   const char* env_backend = std::getenv("KOINOS_VM_BACKEND");
+   if( env_backend == nullptr )
+      return default_vm_backend;
+
+   std::string env_backend_str = env_backend;
+
+   if( env_backend_str.length() == 0 )
+      return default_vm_backend;
+
+   return env_backend_str;
+}
+
 std::shared_ptr< vm_backend > get_vm_backend( const std::string& name )
 {
-   const std::string& target_name = (name != "") ? name : default_vm_backend;
+   const std::string& target_name = (name != "") ? name : get_default_vm_backend_name();
 
    std::vector< std::shared_ptr< vm_backend > > backends = get_vm_backends();
    for( std::shared_ptr< vm_backend > b : backends )
