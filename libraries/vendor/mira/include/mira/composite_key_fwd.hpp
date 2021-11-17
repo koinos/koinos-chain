c00cb8f585930a7d08c6904c8a2860ecd8d8b2f0
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-11-16 16:54:40 -0800

    Remove mira #512

--- libraries/vendor/mira/include/mira/composite_key_fwd.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <boost/tuple/tuple.hpp>
-
-namespace mira { namespace multi_index {
-
-template< typename T > struct composite_key_result;
-
-} } // mira::multi_index
--- libraries/vendor/mira/include/mira/database_configuration.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <nlohmann/json.hpp>
-
-namespace mira::utilities {
-
-nlohmann::json default_database_configuration();
-
-} // mira::utilities
-
