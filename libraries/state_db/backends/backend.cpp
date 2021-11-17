a366cbacebe54e9dc3c809c5963a305b97adba1b
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-11-16 16:54:30 -0800

    Add missing state_db files

---
+++ libraries/state_db/backends/backend.cpp
@@ -0,0 +1,10 @@
+#include <koinos/state_db/backends/backend.hpp>
+
+namespace koinos::state_db::backends {
+
+bool abstract_backend::empty()const
+{
+   return size() == 0;
+}
+
+} // koinos::state_db::backends
