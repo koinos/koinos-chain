c4792355f813f96eb38cbf9fe62f97f719a0b10b
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-05-05 12:34:19 -0700

    Remove extranneous log

--- tests/tests/thunk_test.cpp
+++ tests/tests/thunk_test.cpp
@@ -170,8 +170,6 @@ struct thunk_fixture
       entry->set_value( util::converter::as< std::string >( cbr ) );
       *entry->mutable_space() = chain::state::space::metadata();
 
-      LOG(info) << *entry;
-
       entry = _genesis_data.add_entries();
       entry->set_key( chain::state::key::block_hash_code );
       entry->set_value( util::converter::as< std::string >( unsigned_varint{ std::underlying_type_t< crypto::multicodec >( crypto::multicodec::sha2_256 ) } ) );
