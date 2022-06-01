b22b1b77417112f3cda0461825ece0654fb33ec9
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-06-01 16:24:15 -0700

    Check error codes in tests where applicable #668

--- tests/tests/controller_test.cpp
+++ tests/tests/controller_test.cpp
@@ -14,6 +14,7 @@
 #include <koinos/util/base58.hpp>
 
 #include <koinos/tests/contracts.hpp>
+#include <koinos/tests/util.hpp>
 
 #include <koinos/contracts/token/token.pb.h>
 
@@ -266,7 +267,7 @@ BOOST_AUTO_TEST_CASE( submission_tests )
 
    sign_block( *block_req.mutable_block(), foo_key );
 
-   BOOST_CHECK_THROW( _controller.submit_block( block_req ), chain::invalid_signature_exception );
+   KOINOS_CHECK_THROW( _controller.submit_block( block_req ), chain::invalid_signature );
 
    BOOST_TEST_MESSAGE( "Error when previous block does not match" );
 
@@ -586,15 +587,7 @@ BOOST_AUTO_TEST_CASE( read_contract_tests )
    BOOST_TEST_MESSAGE( "Test read contract db write" );
 
    request.set_contract_id( util::converter::as< std::string >( key3.get_public_key().to_address_bytes() ) );
-   try
-   {
-      _controller.read_contract( request );
-      BOOST_FAIL( "expected reversion_exception not thrown" );
-   }
-   catch ( const chain::reversion_exception& e )
-   {
-      BOOST_REQUIRE_EQUAL( e.get_code(), chain::read_only_context );
-   }
+   KOINOS_REQUIRE_THROW( _controller.read_contract( request ), chain::read_only_context );
 } KOINOS_CATCH_LOG_AND_RETHROW(info) }
 
 BOOST_AUTO_TEST_CASE( transaction_reversion_test )
