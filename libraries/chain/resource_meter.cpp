c639ec7a587bb3fa26d21ce5cd5c87f2e3f51c44
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-06-06 15:44:33 -0700

    Track system resource usage #684

--- libraries/chain/resource_meter.cpp
+++ libraries/chain/resource_meter.cpp
@@ -27,10 +27,13 @@ resource_meter::~resource_meter() = default;
 
 void resource_meter::set_resource_limit_data( const resource_limit_data& rld )
 {
-   _resource_limit_data         = rld;
-   _disk_storage_remaining      = _resource_limit_data.disk_storage_limit();
-   _network_bandwidth_remaining = _resource_limit_data.network_bandwidth_limit();
-   _compute_bandwidth_remaining = _resource_limit_data.compute_bandwidth_limit();
+   _resource_limit_data           = rld;
+   _disk_storage_remaining        = _resource_limit_data.disk_storage_limit();
+   _system_disk_storage_used      = 0;
+   _network_bandwidth_remaining   = _resource_limit_data.network_bandwidth_limit();
+   _system_network_bandwidth_used = 0;
+   _compute_bandwidth_remaining   = _resource_limit_data.compute_bandwidth_limit();
+   _system_compute_bandwidth_used = 0;
 }
 
 void resource_meter::use_disk_storage( uint64_t bytes )
@@ -43,16 +46,20 @@ void resource_meter::use_disk_storage( uint64_t bytes )
       KOINOS_ASSERT( rc_cost <= std::numeric_limits< uint64_t >::max(), reversion_exception, "rc overflow" );
       session->use_rc( rc_cost.convert_to< uint64_t >() );
    }
+   else
+   {
+      _system_disk_storage_used += bytes;
+   }
 
    _disk_storage_remaining -= bytes;
 }
 
-uint64_t resource_meter::disk_storage_used()
+uint64_t resource_meter::disk_storage_used() const
 {
    return _resource_limit_data.disk_storage_limit() - _disk_storage_remaining;
 }
 
-uint64_t resource_meter::disk_storage_remaining()
+uint64_t resource_meter::disk_storage_remaining() const
 {
    if ( auto session = _session.lock() )
    {
@@ -67,6 +74,11 @@ uint64_t resource_meter::disk_storage_remaining()
    return _disk_storage_remaining;
 }
 
+uint64_t resource_meter::system_disk_storage_used() const
+{
+   return _system_disk_storage_used;
+}
+
 void resource_meter::use_network_bandwidth( uint64_t bytes )
 {
    KOINOS_ASSERT( bytes <= _network_bandwidth_remaining, network_bandwidth_limit_exceeded, "network bandwidth limit exceeded" );
@@ -77,16 +89,20 @@ void resource_meter::use_network_bandwidth( uint64_t bytes )
       KOINOS_ASSERT( rc_cost <= std::numeric_limits< uint64_t >::max(), reversion_exception, "rc overflow" );
       session->use_rc( rc_cost.convert_to< uint64_t >() );
    }
+   else
+   {
+      _system_network_bandwidth_used += bytes;
+   }
 
    _network_bandwidth_remaining -= bytes;
 }
 
-uint64_t resource_meter::network_bandwidth_used()
+uint64_t resource_meter::network_bandwidth_used() const
 {
    return _resource_limit_data.network_bandwidth_limit() - _network_bandwidth_remaining;
 }
 
-uint64_t resource_meter::network_bandwidth_remaining()
+uint64_t resource_meter::network_bandwidth_remaining() const
 {
    if ( auto session = _session.lock() )
    {
@@ -101,6 +117,11 @@ uint64_t resource_meter::network_bandwidth_remaining()
    return _network_bandwidth_remaining;
 }
 
+uint64_t resource_meter::system_network_bandwidth_used() const
+{
+   return _system_network_bandwidth_used;
+}
+
 void resource_meter::use_compute_bandwidth( uint64_t ticks )
 {
    KOINOS_ASSERT( ticks <= _compute_bandwidth_remaining, compute_bandwidth_limit_exceeded, "compute bandwidth limit exceeded" );
@@ -111,16 +132,20 @@ void resource_meter::use_compute_bandwidth( uint64_t ticks )
       KOINOS_ASSERT( rc_cost <= std::numeric_limits< uint64_t >::max(), reversion_exception, "rc overflow" );
       session->use_rc( rc_cost.convert_to< uint64_t >() );
    }
+   else
+   {
+      _system_compute_bandwidth_used += ticks;
+   }
 
    _compute_bandwidth_remaining -= ticks;
 }
 
-uint64_t resource_meter::compute_bandwidth_used()
+uint64_t resource_meter::compute_bandwidth_used() const
 {
    return _resource_limit_data.compute_bandwidth_limit() - _compute_bandwidth_remaining;
 }
 
-uint64_t resource_meter::compute_bandwidth_remaining()
+uint64_t resource_meter::compute_bandwidth_remaining() const
 {
    if ( auto session = _session.lock() )
    {
@@ -133,6 +158,11 @@ uint64_t resource_meter::compute_bandwidth_remaining()
    }
 
    return _compute_bandwidth_remaining;
+}
+
+uint64_t resource_meter::system_compute_bandwidth_used() const
+{
+   return _system_compute_bandwidth_used;
 }
 
 void resource_meter::set_session( std::shared_ptr< abstract_rc_session > s )
