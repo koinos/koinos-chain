2e3f0a0e9fc484a3e159daa60c417f975ffacf95
Steve Gerbino <steve@gerbino.co>
2022-10-05 13:17:26 -0400

    #755: Changed warning to error if the node has been finalized already

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -444,7 +444,15 @@ rpc::chain::submit_block_response controller_impl::submit_block(
    }
    catch ( koinos::exception& e )
    {
-      LOG(warning) << "Block application failed - Height: " << block_height << " ID: " << block_id << ", with reason: " << e.what();
+      if ( block_node && !block_node->is_finalized() )
+      {
+         _db.discard_node( block_node->id(), db_lock );
+         LOG(warning) << "Block application failed - Height: " << block_height << " ID: " << block_id << ", with reason: " << e.what();
+      }
+      else
+      {
+         LOG(error) << "Block application failed after finalization - Height: " << block_height << " ID: " << block_id << ", with reason: " << e.what();
+      }
 
       if ( _client )
       {
@@ -461,20 +469,18 @@ rpc::chain::submit_block_response controller_impl::submit_block(
       if ( std::holds_alternative< protocol::block_receipt >( ctx.receipt() ) )
          e.add_json( "logs", std::get< protocol::block_receipt >( ctx.receipt() ).logs() );
 
-      if ( block_node && !block_node->is_finalized() )
-      {
-         _db.discard_node( block_node->id(), db_lock );
-      }
-
       throw;
    }
    catch ( ... )
    {
-      LOG(warning) << "Block application failed - Height: " << block_height << ", ID: " << block_id << ", for an unknown reason";
-
       if ( block_node && !block_node->is_finalized() )
       {
          _db.discard_node( block_node->id(), db_lock );
+         LOG(warning) << "Block application failed - Height: " << block_height << ", ID: " << block_id << ", for an unknown reason";
+      }
+      else
+      {
+         LOG(error) << "Block application failed after finalization - Height: " << block_height << ", ID: " << block_id << ", for an unknown reason";
       }
 
       throw;
