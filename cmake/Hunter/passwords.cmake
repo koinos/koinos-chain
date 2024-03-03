9d1a97827c1667c68cc5b83d51a92a98ca78f33e
Steve Gerbino <steve@gerbino.co>
2024-03-03 20:53:17 +0100

    Initial migration to pitchfork specification

--- cmake/Hunter/passwords.cmake
+++
@@ -1,8 +0,0 @@
-# cmake/Hunter/passwords.cmake
-
-hunter_upload_password(
-    REPO_OWNER "koinos"
-    REPO "hunter-cache"
-    USERNAME "koinos-ci"
-    PASSWORD "$ENV{GITHUB_USER_PASSWORD}"
-)
--- tests/tests/main.cpp
+++
@@ -1,3 +0,0 @@
-#define BOOST_TEST_MODULE koinos_tests
-#include <BoostTestTargetConfig.h>
-#include <boost/test/included/unit_test.hpp>
